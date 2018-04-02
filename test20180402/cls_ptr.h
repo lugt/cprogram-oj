9232f0309d421c912923d0815396badb9d6fd549
Fruit of Eden <lu.gt@163.com>
2018-04-02 20:48:49 +0800

    Sim

--- test20180402/cls_ptr.h
+++ test20180402/cls_ptr.h
@@ -4,51 +4,49 @@
 
 #ifndef TEST20180402_CLS_PTR_H
 #define TEST20180402_CLS_PTR_H
+
 #include<iostream>
 
 using namespace std;
-class Student
-{
+
+class Student {
 private:
-    string name;
     string sex;
     long number;
     string collage;
     long tel;
 public:
-    void NAME(string name_1)
-    { name = name_1; }
-    void SEX(string sex_1)
-    { sex = sex_1; }
-    long NUMBER(long number_1)
-    { return number_1; }
-    void COLLAGE(string collage_1)
-    { collage = collage_1; }
-    long TEL(long tel_1)
-    { return tel_1; }
+    void NAME(string name_1) { name = name_1; }
+
+    void SEX(string sex_1) { sex = sex_1; }
+
+    long NUMBER(long number_1) { return number_1; }
+
+    void COLLEGE(string collage_1) { collage = collage_1; }
+
+    long TEL(long tel_1) { return tel_1; }
+
+    string name;
 };
-void Sort(Student ptr[], int n)
-{
+
+void Sort(Student ptr[], int n) {
     int i, j;
-    Student temp.NAME;
+    string name_temp;
     for (j = 0; j < n - 1; j++)
-        for (i = 0; i < n - 1 - j; i++)
-        {
-            if(ptr[i].NAME < ptr[i + 1].NAME);
+        for (i = 0; i < n - 1 - j; i++) {
+            if (ptr[i].name.compare(ptr[i + 1].name));
             {
-                temp.NAME =ptr[i].NAME;
-                ptr[i].NAME = ptr[i+1].NAME;
-                ptr[i+1].NAME = temp.NAME ;
+                name_temp = ptr[i].name;
+                ptr[i].name = ptr[i + 1].name;
+                ptr[i + 1].name = name_temp;
             }
         }
 }
 
 
-
-int main(void)
-{
+int main(void) {
     int t;
-    cin>>t;
+    cin >> t;
     Student *sb;
     string name_2;
     string sex_2;
@@ -56,17 +54,29 @@ int main(void)
     string collage_2;
     long tel_2;
     sb = new Student[t];
-    for(int i = 0 ; i<t; i++)
-    {
-        cin>>sb[i].NAME(name_2)
-           >>sb[i].SEX(sex_2)
-           >>sb[i].NUMBER(number_2)
-           >>sb[i].COLLAGE(collage_2)
-           >>sb[i].TEL(tel_2);
+
+    for (int i = 0; i < t; i++) {
+        string str;
+        cin >> str;
+        sb[i].NAME(str);
+
+        cin >> str;
+        sb[i].SEX(str);
+
+        cin >> tel_2;
+        sb[i].NUMBER(tel_2);
+
+        cin >> str;
+        sb[i].COLLEGE(str);
+
+        cin >> tel_2;
+        sb[i].TEL(tel_2);
     }
-    Sort(sb , t);
-    for(int i = 0; i<t; i++)
-        cout<<sb[i].NAME<<endl;
+
+    Sort(sb, t);
+
+    for (int i = 0; i < t; i++)
+        cout << sb[i].name << endl;
     return 0;
 }
 
