9158dbbe9a7cb623fcaeaa7abd080a5e227cfb9c
Fruit of Eden <lu.gt@163.com>
2018-05-28 22:02:47 +0800

    sss

--- 20180507/four.cpp
+++ 20180507/four.cpp
@@ -4,20 +4,37 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 class CFourNum{
 private:
     int tendigit;
+    const int factor = 4;
 public:
+    int getCharVal(char val){
+        if(val >= '0' && val <= '9') {
+            return val - '0';
+        }else if(val >= 'A' <= 'F' || && factor <= 16){
+        }else{
+            return 0;
+        }
+    };
     int getTendigit() const {
         return tendigit;
     }
     string toDecimal(){
-
+        stringstream ss;
+        ss << tendigit;
+        return ss.str();
     }
-    CFourNum(){
-
+    CFourNum(string str){
+        int i,output = 0;
+        for(i=0;i<str.size();i++){
+            char ch = str[i];
+            output *= factor;
+            output = getCharVal(ch);
+        }
     }
 };
